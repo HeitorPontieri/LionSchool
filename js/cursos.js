422bc3364464691626a678f22fda776691e63080
HeitorPontieri <98404376+HeitorPontieri@users.noreply.github.com>
2022-09-15 08:55:09 -0300

    inicio projeto

---
+++ js/cursos.js
@@ -0,0 +1,14 @@
+var cursos = [
+    {
+        "nome"  :   "001 - Técnico em Desenvolvimento de Sistemas",
+        "sigla" :   "DS",
+        "icone" :   "https://image.shutterstock.com/image-vector/api-interface-vector-icon-600w-659203513.jpg",
+        "carga" :   "1200",
+    },
+    {
+        "nome"  :   "002 - Técnico em Redes de Computadores",
+        "sigla" :   "RDS",
+        "icone" :   "https://img.icons8.com/ultraviolet/344/thin-client.png",
+        "carga" :   "1200"
+    }
+];
